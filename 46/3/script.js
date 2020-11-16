b447aa85452ace691083dd69da92254a93745b3c
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-16 12:26:19 +0100

    solve-46-3

---
+++ 46/3/script.js
@@ -0,0 +1,10 @@
+let animal = {
+    species: "Dog",
+    name: "Dogge",
+    age: 69
+}
+
+animal.age++
+animal.age = animal.age + 1
+// det här är två olika sätt få djuret att åldras (nu kommer åldern öka med 2)
+console.log(animal)
