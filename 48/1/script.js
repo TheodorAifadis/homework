892d3c0e41dc7e4a17b4962f9608043461686aa9
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-23 14:10:02 +0100

    48-1

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,11 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
 
+const name = 'Theo'
+
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+
+let i = 0
+while (i < 500) {
+    console.log(`Hejsan ${name}!!`)
+    i++
+}
