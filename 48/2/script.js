1b9488c3c53a7528d9de77ec305ed883f4892a79
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-29 20:35:57 +0100

    48-2

--- 48/2/script.js
+++ 48/2/script.js
@@ -2,6 +2,14 @@
 // Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
 // Om talet blir 6, avsluta loopen
 
+let x = Math.floor(Math.random() * 10) + 1
+while (x !=6) {
+    console.log(x)
+    x = Math.floor(Math.random() * 10) + 1
+}
+
+// jag loggar ut x för att se siffran 6 i slutet
+console.log(x)
 
 // Exempel på output:
 // 4
