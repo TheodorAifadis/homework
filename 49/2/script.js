390e481490dfa38c064b2b65e1df29e9d08313e6
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-12-06 16:06:20 +0100

    49-1

---
+++ 49/2/script.js
@@ -0,0 +1,20 @@
+let str1 = ''
+
+for (let i = 0; i < 10; i++) {
+    str1 = str1 + '*'.repeat(i)
+    str1 = str1 + '\n'  
+}
+
+console.log(str1)
+
+
+
+// kvadraten
+let str2 = ''
+
+for (let i = 0; i < 5; i++) {
+    str2 = str2 + '* '.repeat(5)
+    str2 = str2 + '\n'
+}
+
+console.log(str2)
